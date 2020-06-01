96e63a79fae561a7b78084f8d1d2e86052dad0bb
Alex Tsarenkov <alexey.tsarenkov@yandex.ru>
2020-06-01 13:16:50 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,5 @@
 const cors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080/');
+  res.header('Access-Control-Allow-Origin', 'http://localhost');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
